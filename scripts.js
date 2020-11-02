6bf6fe2a709862434ee014ec656c564eace77966
steph jarrett <s.jatter12345@gmail.com>
2020-11-01 20:02:54 -0500

    added everything

--- scripts.js
+++ scripts.js
@@ -1,1 +1,14 @@
 /* Add your JavaScript to this file */
+window.onload = function() {
+    var btn = document.querySelector("button");
+    var msg = document.getElementsByClassName("message");
+    btn.addEventListener("click", function(e){
+        e.preventDefault();
+        var text = document.getElementById("email").value;
+        if (text != ""){
+            msg[0].innerHTML = "Thank you your email address " + text +  " has been added to our mailing list";
+        }else {
+            msg[0].innerHTML = "Please enter a valid email address";
+        }
+    });
+}
